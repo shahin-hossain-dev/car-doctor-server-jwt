e4dd12ba36cf59dbc4fa3965ccf49293f4813298
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-05-01 23:42:17 +0600

    get data from database

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollection = client.db("carDoctorDB").collection("services");
+
+    // get services data
+    app.get("/services", async (req, res) => {
+      const cursor = await servicesCollection.find().toArray();
+      res.send(cursor);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
